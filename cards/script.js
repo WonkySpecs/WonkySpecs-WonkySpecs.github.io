d5f0a703232691416293f46f0a7f0518edc64731
Will Taylor <w1ll100@hotmail.co.uk>
2021-03-29 00:56:05 +0100

    malifaux suits first pass

--- cards/script.js
+++ cards/script.js
@@ -1,8 +1,9 @@
 function cardValues() {
-    const vals  = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                  "J", "Q", "K"];
-    const suits = ["C", "D", "H", "S"];
-    return suits.flatMap(s => vals.map(v => v + s)).concat(["RJ", "BJ"]);
+    const suits = ["clubs", "diamonds", "hearts", "spades"]
+    const vals  = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                  "Jack", "Queen", "King"];
+    return suits.flatMap(s => vals.map(v => ({ suit: s, val: v })))
+        .concat(["Red Joker", "Black Joker"]);
 }
 
 function createDeck() {
@@ -26,12 +27,20 @@ function draw(deck) {
     return drawn;
 }
 
-function getInfoString(deck) {
+function cardString(card, suitMapping) {
+    if (card.suit === undefined) return card;
+    const suitStr = suitMapping[card.suit] === undefined
+        ? card.suit
+        : suitMapping[card.suit];
+    return card.val + " of " + suitStr;
+}
+
+function getInfoString(deck, suitMapping) {
     const lastDraw = deck.drawn.length > 0 
         ? deck.drawn[deck.drawn.length - 1] 
         : false;
     const drawMessage = lastDraw
-        ? ", drew: " + lastDraw
+        ? ", drew: " + cardString(lastDraw, suitMapping)
         : "";
     return "Cards remaining: " + deck.undrawn.length + drawMessage;
 }
@@ -42,7 +51,9 @@ function createBtn(text) {
     return b;
 }
 
-function createDeckContainer(parElem) {
+function createDeckContainer(initSuitMapping) {
+    let suitMapping = initSuitMapping;
+    console.log(suitMapping);
     const grouping = document.createElement("div");
     grouping.classList.add("deck-container");
 
@@ -56,29 +67,66 @@ function createDeckContainer(parElem) {
     const drawBtn = createBtn("Draw");
     const reshuffleBtn = createBtn("Reshuffle");
     const info = document.createElement("div");
-    info.innerHTML = getInfoString(deck);
+    info.innerHTML = getInfoString(deck, initSuitMapping);
 
+    let reshuffled = false;
     drawBtn.onclick = _ => {
         const drew = draw(deck);
-        info.innerHTML = getInfoString(deck)
+        info.innerHTML = getInfoString(deck, suitMapping)
+        reshuffled = false;
     };
 
     reshuffleBtn.onclick = _ => {
         resetDeck(deck);
         info.innerHTML = "Reshuffled";
+        reshuffled = true;
     };
 
     grouping.appendChild(nameContainer);
     grouping.appendChild(drawBtn);
     grouping.appendChild(reshuffleBtn);
     grouping.appendChild(info);
-    parElem.appendChild(grouping);
+    return {
+        rootElem: grouping,
+        redraw: sm => {
+            suitMapping = sm;
+            if (!reshuffled) info.innerHTML = getInfoString(deck, sm);
+        }
+    };
 }
+
+function getActiveSuitMapping() {
+    const standard = {
+        "clubs": "Clubs",
+        "diamonds": "Diamonds",
+        "hearts": "Hearts",
+        "spades": "Spades",
+    };
+    const malifaux = {
+        "clubs": "Tomes",
+        "diamonds": "Masks",
+        "hearts": "Rams",
+        "spades": "Crows",
+    };
+
+    const isMalifaux = document.getElementById("malifauxCheckbox").checked;
+    return isMalifaux ? malifaux : standard;
+}
+
 
 window.onload = w => {
     const container = document.querySelector("main");
-    document.querySelector("#add-deck-btn").onclick = _ => {
-        createDeckContainer(container);
+    const decks = [];
+    const addDeck = () => {
+        const deckContainer = createDeckContainer(getActiveSuitMapping());
+        decks.push(deckContainer);
+        container.appendChild(deckContainer.rootElem);
     }
-    createDeckContainer(container);
+    document.getElementById("addDeckBtn").onclick = _ => {
+        addDeck();
+    }
+    addDeck();
+    malifauxCheckbox.addEventListener('change', ev => {
+        decks.forEach(d => d.redraw(getActiveSuitMapping()));
+    });
 };
