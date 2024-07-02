62b4793e86a318f30023e08497582f058bab8ec6
Will Taylor <w1ll100@hotmail.co.uk>
2024-07-02 17:37:36 +0100

    Add Sargent post, and preview images

---
+++ scripts/previewImages.js
@@ -0,0 +1,9 @@
+function displayPreview(id) {
+    const preview = document.getElementById(id);
+    preview.style.visibility = "visible";
+}
+
+function closePreview(id) {
+    const preview = document.getElementById(id);
+    preview.style.visibility = "hidden";
+}
